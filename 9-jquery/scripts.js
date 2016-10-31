78ed12dffeffe2db5d304f102569219c7dfaed92
jakeiwen <iwenx006@umn.edu>
2016-10-31 16:06:00 -0500

    semicolon

--- 9-jquery/scripts.js
+++ 9-jquery/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 var $el = $('section');
 $el.append('<p>Page Loaded</p>');
 	// 2 - Change the text of the h1 element to something (anything!) else.
-$('h1').text('Something else')
+$('h1').text('Something else');
   // 3 - Get rid of (delete) the useless unordered list (ul) by first
   // selecting the h2 nested within its list item.
 $('h2').parent().parent().remove();
